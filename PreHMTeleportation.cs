0688e33818ed23f6de5ae2bae0415d34f08814ad
TheGoogolMoon <67603831+TheGoogolMoon@users.noreply.github.com>
2020-07-02 04:40:45 +0300

    Add missing updated file

--- PreHMTeleportation.cs
+++ PreHMTeleportation.cs
@@ -4,15 +4,18 @@ using static Terraria.ModLoader.ModContent;
 using System.Collections.Generic;
 using System.Linq;
 using System;
-using Steamworks;
-using Terraria.ID;
+using Terraria.UI;
+using Microsoft.Xna.Framework;
+using PreHMTeleportation.UI;
+using Terraria.GameInput;
+using Terraria.ModLoader.IO;
 
 namespace PreHMTeleportation
 {
 	public class PHMTPlayer : ModPlayer
     {
         public readonly Dictionary<(int x, int y), string> transporterLocations = new Dictionary<(int x, int y), string>();
-        //private readonly Dictionary<(int x, int y), string> transporterLocationNames = new Dictionary<(int x, int y), string>();
+        public readonly Dictionary<(int x, int y), string> customTransporterNames = new Dictionary<(int x, int y), string>();
         public (int x, int y) chosenTransporter = (0, 0);
 
         public void UpdateTransporters()
@@ -27,8 +30,8 @@ namespace PreHMTeleportation
                     Tile tile = Main.tile[x, y];
                     if (tile != null && tile.type == TileType<Tiles.Transporter>())
                     {
-                        //if (transporterLocationNames.ContainsKey((x, y))) transporterLocations.Add((x, y), transporterLocationNames[(x, y)]); //else
-                        transporterLocations.Add((x, y), index++.ToString());
+                        if (customTransporterNames.ContainsKey((x, y))) transporterLocations.Add((x, y), customTransporterNames[(x, y)]);
+                        else transporterLocations.Add((x, y), index++.ToString());
                     }
                 }
             }
@@ -47,14 +50,117 @@ namespace PreHMTeleportation
             }
         }
 
+        public override void ProcessTriggers(TriggersSet triggersSet)
+        {
+            if (PreHMTeleportation.TUIHotKey.JustPressed)
+            {
+                PreHMTeleportation.ToggleTUI();
+            }
+        }
+
         public override void OnEnterWorld(Player player)
         {
             UpdateTransporters();
             base.OnEnterWorld(player);
         }
+
+        public override void Load(TagCompound tag)
+        {
+            if (tag.ContainsKey("TransporterNames")) ApplyTransporterNamesFromSave(tag.GetList<string>("TransporterNames"));
+        }
+
+        private void ApplyTransporterNamesFromSave(IList<string> names)
+        {
+            foreach (string name in names)
+            {
+                var data = name.Split(' ');
+                if (data.Length == 3 && int.TryParse(data[0], out int x) && int.TryParse(data[1], out int y)) customTransporterNames[(x, y)] = data[2];
+            }
+        }
+
+        public override TagCompound Save()
+        {
+            //not the best solution, but a string of the form "x y name" works well enough here
+            //another approach would be separate TagCompound entries and Zip them together
+            var names = new List<string>();
+            foreach (var pair in customTransporterNames)
+            {
+                names.Add("" + pair.Key.x + " " + pair.Key.y + " " + pair.Value);
+            }
+
+            return new TagCompound
+            {
+                ["TransporterNames"] = names
+            };
+        }
     }
 
 	public class PreHMTeleportation : Mod
 	{
+        public static ModHotKey TUIHotKey;
+
+        private static UserInterface _transporterUserInterface;
+        internal static TUINameSetter TransporterNameUI;
+
+        public override void Load()
+        {
+            TUIHotKey = RegisterHotKey("Transporter UI", "T");
+
+            if (!Main.dedServ)
+            {
+                _transporterUserInterface = new UserInterface();
+                _transporterUserInterface.SetState(null);
+            }
+        }
+
+        public override void Unload()
+        {
+            TUIHotKey = null;
+            TransporterNameUI = null;
+            _transporterUserInterface = null;
+        }
+
+        public static void ToggleTUI()
+        {
+            if (_transporterUserInterface.CurrentState == null) OpenTNameUI();
+            else CloseTNameUI();
+        }
+
+        private static void OpenTNameUI()
+        {
+            if (!Main.dedServ)
+            {
+                TransporterNameUI = new TUINameSetter();
+                _transporterUserInterface.SetState(TransporterNameUI);
+            }
+        }
+
+        private static void CloseTNameUI()
+        {
+            if (!Main.dedServ) _transporterUserInterface.SetState(null);
+        }
+
+        public override void UpdateUI(GameTime gameTime)
+        {
+            if (_transporterUserInterface.CurrentState != null) _transporterUserInterface?.Update(gameTime);
+        }
+
+        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
+        {
+            int mouseTextIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text"));
+            if (mouseTextIndex != -1)
+            {
+                layers.Insert(mouseTextIndex, new LegacyGameInterfaceLayer
+                    (
+                    "TransporterUI: Mouse Text",
+                    delegate
+                    {
+                        _transporterUserInterface.Draw(Main.spriteBatch, new GameTime());
+                        return true;
+                    },
+                    InterfaceScaleType.UI)
+                );
+            }
+        }
     }
 }
